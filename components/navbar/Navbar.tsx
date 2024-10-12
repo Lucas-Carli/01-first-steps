b25555fb4f817db50b147bc3a4f8e56202a1af89
lucas carli <lucas.carli@tecso.coop>
2024-10-12 13:08:49 -0300

    feat: finish section 2

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,23 +1,33 @@
-// const temporalAsync = () => {
-//     return new Promise ( (resolve) => {
-//         setTimeout(() => {
-//             resolve(true);
-//         }, 2000 );
-//     })
-// }
+import { HomeIcon } from "@primer/octicons-react"
+import Link from "next/link"
+import { ActiveLink } from "../active-link/ActiveLink"
 
-// export const Navbar = async() => {
+const navItems = [
+    { path: '/about', text: 'About' },
+    { path: '/pricing', text: 'Pricing' },
+    { path: '/contact', text: 'Contact' },
+
+]
 
 export const Navbar = () => {
 
-// await temporalAsync();
     return (
         <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded" >
-            <span>Home</span>
+
+            <Link href={'/'} className='flex-items-center'>
+                <HomeIcon className="mr-2" />
+                <span>Home</span>
+            </Link>
+
             <div className="flex flex-1"></div>
-            <a className="mr-2" href="/about">About</a>
-            <a className="mr-2" href="/pricing">Pricing</a>
-            <a className="mr-2" href="/contact">Contact</a>
+
+            {
+                navItems.map(navItem => (
+                    // <Link key={navItem.path} className="mr-2" href={navItem.path}>{navItem.text}</Link>
+                <ActiveLink key={navItem.path} { ...navItem}/> // Toma las propiedades que necesita del navItem
+                ))
+            }
+
         </nav>
     )
 }
